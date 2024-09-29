9bf50ef3ca3a34b9ca9cf4e242ffa6186ac6eb23
Rissochek <146011723+Rissochek@users.noreply.github.com>
2024-09-29 14:08:42 +0300

    Update main.c

--- lab1/main.c
+++ lab1/main.c
@@ -6,72 +6,6 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-bool check_on_zeros(char* data, int size){
-    for (int i = 1; i < size; i++){
-        if (data[i] == '0'){
-            if (data[i+1] == ' ' || data[i+1] == '\0' || data[i+1] == '\n'){
-                if (data[i-1] == ' '){
-                    return true;
-                }
-            }
-            if (data[i+1] == '.' || data[i+1] == ','){
-                int non_zeros_counter = 0;
-                i += 2;
-                while(data[i] != ' ' && data[i] != '\0'){
-                    char sth = data[i++];
-                    if (sth != '0'){
-                        if (sth != '\0'){
-                            if (sth != '\n'){
-                                non_zeros_counter += 1;
-                            }
-                        }
-                    }
-                }
-                if (non_zeros_counter == 0){
-                    return true;
-                }
-                non_zeros_counter = 0;
-            }
-        }
-    }
-    return false;
-}
-
-float calc_func(char* data, int size){
-    char *buffer = malloc(sizeof(char) * 100);
-    int j = 0;
-    char ch;
-    float first_number;
-    bool flag = false;
-    float number = 1;
-    float tmp = 0;
-    for (int i = 0; i < size; i++){
-        if (data[i] != '\0'){
-            while ((ch = data[i]) != ' ' && ch != '\n' && ch != '\0') {
-                buffer[j++] = ch;
-                i++;
-            }
-            if (flag == false){
-                sscanf(buffer, "%f", &first_number);
-                number = first_number;
-                flag = true;
-            }else{
-                sscanf(buffer, "%f", &tmp);
-                number = number / tmp;
-            }
-            j = 0;
-            while (buffer[j] != '\0'){
-                buffer[j++] = ' ';
-            }
-            j = 0;
-        }else{
-            break;
-        }
-    }
-    free(buffer);
-    return number;
-}
-
 int main(int argc, char *argv[])
 {
     int    pipefd_from_parent_to_child[2];
@@ -106,54 +40,20 @@ int main(int argc, char *argv[])
 
     if (cpid == 0) {    
         close(pipefd_from_parent_to_child[1]);          
-        close(pipefd_from_child_to_parent[0]); 
-        int i = 0;
-        char ch;
-        while (read(pipefd_from_parent_to_child[0], &buf, 1) > 0){
-                if (buf != '|'){
-                filename[i++] = buf;
-                } else{
-                    break;
-                }
-        }
-        while (read(pipefd_from_parent_to_child[0], &buf, 1) > 0){
-            if (counter < size){
-                    input_data[counter++] = buf;
-                } else{
-                    size *= 2;
-                    char *buffer = realloc(input_data, size);
-                    if (buffer == NULL) {
-                        perror("realloc failed");
-                        free(input_data); 
-                        _exit(EXIT_FAILURE);
-                    }else{
-                        input_data = buffer;
-                        input_data[counter++] = ch;
-                    }
-                }
-        }
-        input_data[counter] = '\0';
-        float tmp;
-        if (check_on_zeros(input_data, size) == 0){
-            tmp = calc_func(input_data, size);
-        } else{
-            perror("Cannot devide by zero\n");
-            free(input_data);
-            _exit(EXIT_FAILURE);
-        }
+        close(pipefd_from_child_to_parent[0]);
 
-        filename[i] = '\0';
-        FILE *fptr;
-        fptr = fopen(filename, "w");
-        fprintf(fptr, "%f", tmp);
-        fclose(fptr);
-        
+        dup2(pipefd_from_parent_to_child[0], STDIN_FILENO);
+        dup2(pipefd_from_child_to_parent[1], STDOUT_FILENO);
 
         close(pipefd_from_parent_to_child[0]);
-        close(pipefd_from_child_to_parent[1]);
-        free(input_data);
-        _exit(EXIT_SUCCESS);
-
+        close(pipefd_from_child_to_parent[1]); 
+        char *args[] = {"./child", NULL};
+        if (execv(args[0], args) == -1) {
+            perror("execv");
+            exit(EXIT_FAILURE);
+        }
+        close(pipefd_from_parent_to_child[0]);          
+        close(pipefd_from_child_to_parent[1]); 
     } else {            
         close(pipefd_from_parent_to_child[0]);         
         close(pipefd_from_child_to_parent[1]);
@@ -174,7 +74,6 @@ int main(int argc, char *argv[])
             }
         }
         input_data[counter] = '\0';
-
         write(pipefd_from_parent_to_child[1], argv[1], strlen(argv[1]));
         write(pipefd_from_parent_to_child[1], "|", 1);
         write(pipefd_from_parent_to_child[1], input_data, strlen(input_data));
